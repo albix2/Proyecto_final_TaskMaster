1dd4d961977141499fe5a3d7ae9da59e854acb1b
albix2 <albadelpino1@gmail.com>
2024-05-20 21:28:50 +0200

    alba

--- TaskMaster/www/cerrarSesion.php
+++ TaskMaster/www/cerrarSesion.php
@@ -1,7 +1,7 @@
 <?php 
 	/* Desarrollado por: PROGRAMANDO BROTHERS 	
 	Suscribete a : https://www.youtube.com/ProgramandoBrothers y comparte los vídeos.
-	Recuerda: "EL CONOCIMIENTO SE COMPARTE, POR MÁS POCO QUE SEA".
+	Recuerda: "EL CONOCIMIENTO SE COMPARTE, POR MÁS POCO QUE SEA".gg
 	*/
 	session_start();
 	
